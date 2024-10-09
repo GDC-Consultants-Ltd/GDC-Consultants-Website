eb455bffafe229deb7c2b80ecc3e495863580406
Malshi Kulasinghe <kulasinghe1malshi@gmail.com>
2024-10-09 21:05:24 +0530

    replace services images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     domains: [
       `${process.env.NEXT_PUBLIC_HUBSPOT_PORTAL_ID}.fs1.hubspotusercontent-na1.net`, // Dynamically use the HubSpot portal ID from .env
