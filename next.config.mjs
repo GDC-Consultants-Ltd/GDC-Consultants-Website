e174c8039f5a2c174dfe516990fcff8b7dfa80d5
Malshi Kulasinghe <kulasinghe1malshi@gmail.com>
2024-10-09 22:05:10 +0530

    updated code

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
-    domains: [
-      `${process.env.NEXT_PUBLIC_HUBSPOT_PORTAL_ID}.fs1.hubspotusercontent-na1.net`, // Dynamically use the HubSpot portal ID from .env
-    ],
+    unoptimized: true,
   },
 };
 
